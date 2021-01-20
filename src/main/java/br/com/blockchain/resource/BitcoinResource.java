a78315cfadcb38919f3531c22fe831af6769e293
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2021-01-19 21:47:45 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/blockchain/resource/BitcoinResource.java
@@ -0,0 +1,29 @@
+package br.com.blockchain.resource;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import br.com.blockchain.model.Bitcoin;
+import br.com.blockchain.service.BitcoinService;
+
+@Path("/bitcoins")
+public class BitcoinResource 
+{
+	@Inject
+	@RestClient
+	BitcoinService bitcoinService;
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Bitcoin> listar() 
+	{
+		return bitcoinService.listar();
+	}
+}
